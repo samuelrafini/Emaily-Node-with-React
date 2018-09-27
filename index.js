e33e1352f3eb3e20ddf697835b0cda2dbf3d0ac3
Samuel Rafini <samuelrafini@gmail.com>
2018-09-27 17:53:57 +0200

    google oauth

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({hi: 'There '});
-});
+// app.get('/', (req,res) => {
+//     res.send({hi: 'There '});
+// });
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+}));
 
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
